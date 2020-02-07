91d685bad9a975a70367079bc332bbfdca2b2b02
Marcel Kolarcik <60097861+marcelkolarcik@users.noreply.github.com>
2020-02-07 16:13:54 +0000

    working on map popups and clusters

--- assets/js/open_maps/clusters.js
+++ assets/js/open_maps/clusters.js
@@ -2,11 +2,6 @@ var tiles = L.tileLayer( 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	maxZoom    : 18,
 	attribution: '&copy; <a href="https://openstreetmap.org/copyright" target="_blank">OpenStreetMap</a> contributors, Points &copy 2012 LINZ'
 } );
-//
-// // Cork
-//                 { lat: 53.350140, lng: -6.266155}, // Dublin
-//                 { lat:52.668018, lng: -8.630498}, // Limerick
-//                  { lat: 53.270962, lng: -9.062691} //Galway
 
 var map = L.map( 'map', {
 	center: L.latLng( 44.8, -17.2 ),
@@ -20,12 +15,15 @@ var mcg = L.markerClusterGroup( {
 	                                spiderfyOnMaxZoom: false
                                 } );
 
-for ( var i = 0; i < DB_data.length; i++ ) {
-	var a = DB_data[ i ];
-	var title = a[ 2 ];
-	var marker = L.marker( new L.LatLng( a[ 0 ], a[ 1 ] ), { title: title } );
-	marker.bindPopup( "<img src='../../images/avatar/avatar3.png' alt = 'img'  style='width:30px;height:30px;'><b>Hello Cor666k!</b><br>I am a popup. <a href='#' >click me</a>" );
+for ( var room in properties ) {
+	
+	var property = properties[ room ];
+	
+	var marker = L.marker( new L.LatLng( property.lat, property.lng ), { title: property.p_name } );
+	marker.bindPopup( `<img src="assets/images/avatar/avatar${property.p_id }.png"   alt = "property image"  style="width:30px;height:30px;"><b>${property.city}</b><br>${property.lng} ${property.lat} <a class="property_pop" id="${property.p_id}" href="#" >click me</a>` );
 	mcg.addLayer( marker );
+	
 }
+
 
 map.addLayer( mcg );
---
+++ assets/js/search.js
@@ -0,0 +1,70 @@
+$( document ).on( "click", "#search_btn", function ( e ) {
+	/*we don't want to submit form to the server, because we have none*/
+	e.preventDefault();
+	
+	var city = $( '#city' ).val().toLowerCase();
+	var room_type = $( "#room_type" ).val();
+	var board_type = $( "#board_type" ).val();
+	var search_results = $( '#search_results' );
+	
+	let board_types =[
+		'Single ( En Suite )',
+		'Double ( En Suite )',
+	];
+	
+	let room_types  =  [
+		'Room only',
+		'Bed & Breakfast',
+		'Breakfast & Dinner',
+		'All Inclusive'
+	];
+	
+	
+	search_results.html( '' );
+	
+	
+	console.log(properties[0]);
+	for ( var room in properties ) {
+		
+		var property = properties[ room ];
+		
+		if ( property.city.toString().toLowerCase() === city.toString() &&
+			property.room_type.toString() === room_type.toString() &&
+			property.board_type.toString() === board_type.toString() ) {
+			
+			search_results.append( `
+				<div class="img-thumbnail">
+						<img src="assets/images/avatar/avatar${property.p_id}.png">
+						${property.p_address}  ${room_types[property.room_type]}  ${board_types[property.board_type]}
+						</div>
+			` )
+		}
+	}
+	
+	
+} );
+$( document ).on( "click", ".property_pop", function ( e ) {
+	
+	var search_results = $( '#search_results' );
+	let board_types =[
+		'Single ( En Suite )',
+		'Double ( En Suite )',
+	];
+	
+	let room_types  =  [
+		'Room only',
+		'Bed & Breakfast',
+		'Breakfast & Dinner',
+		'All Inclusive'
+	];
+	search_results.html( '' );
+	
+	var p_id = $(this).attr('id');
+	var property = properties[p_id];
+	search_results.append( `
+				<div class="img-thumbnail">
+						<img src="assets/images/avatar/avatar${property.p_id}.png">
+						${property.p_address}  ${room_types[property.room_type]}  ${board_types[property.board_type]}
+						</div>
+			` )
+});
