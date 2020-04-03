71fbf65654d3cf603bb04ceba08598bcd9dcbdee
Marcel Kolarcik <60097861+marcelkolarcik@users.noreply.github.com>
2020-04-03 09:58:30 +0100

    removed unwanted character from render_language_dropdown.js

--- assets/js/render_language_dropdown.js
+++ assets/js/render_language_dropdown.js
@@ -13,7 +13,7 @@ $('#languages').append(`
 		                          class = "___"
 		                          data-alt = "english flag image"
 		                          src = "assets/images/flags/en_sm.png"
-                          ></a >Á
+                          ></a >
                     <div
 		                    aria-labelledby = "language_flag"
 		                    class = "dropdown-menu"
