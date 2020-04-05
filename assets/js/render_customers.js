8d67e9bdcb692645f4e9095f8b7f6a44f78395e8
Marcel Kolarcik <60097861+marcelkolarcik@users.noreply.github.com>
2020-04-05 12:57:45 +0100

    added total income, and yearly occupancy to customers popup, added translated strings to translation files

--- assets/js/render_customers.js
+++ assets/js/render_customers.js
@@ -9,18 +9,21 @@ import { translate } from "./translator/translator.js";
 			
 			var customers_table = $ ( '#customers_table' );
 			var customers;
-			
+			var total_income = 0;
+			var weeks_booked = [];
 			/*IF ROOM HAS ANY CUSTOMERS => WE WILL DISPLAY CUSTOMERS TO OWNER*/
 			if ( sessionStorage.room_to_edit && ( customers = JSON.parse ( localStorage.CUSTOMERS )[ JSON.parse (
 				sessionStorage.room_to_edit ).p_id ] ) )
 				{
 					/*CREATING CUSTOMER TABLE TO DISPLAY*/
 					customers_table.append ( `<h4 class="___" data-text="Customers"></h4>
+						<p id="room_name" class="bg_orange_light">${JSON.parse (
+						sessionStorage.room_to_edit ).p_address.property_name}</p>
                 <div class="table-responsive">
 				           <table class="table table-sm table-bordered">
 							 <thead class="bg-secondary text-light">
 							    <tr>
-							      <th scope="col" >id</th>
+							     
 							      <th scope="col" class="___" data-text="name"></th>
 							      <th scope="col" class="___" data-text="Email"></th>
 							      <th scope="col" class="___" data-text="weeks"></th>
@@ -36,7 +39,22 @@ import { translate } from "./translator/translator.js";
 						
 						$.each ( customer, function ( index, value )
 						{
+							if(index !== 'p_id')
 							$ ( '#table_row_' + key ).append ( `<td >${ value }</td>` );
+							
+							if(index === 'weeks')
+								{
+									$.each(value.split('-'), function ( key, week )
+									{
+										weeks_booked.push(week);
+									})
+									
+								}
+							
+							if(index === 'total_price')
+								{
+									total_income = parseFloat(total_income)  + parseFloat(value) ;
+								}
 						} );
 						
 						
@@ -46,6 +64,15 @@ import { translate } from "./translator/translator.js";
 					
 					customers_table.append ( ` </tbody></table>
 			                </div>` );
+					
+					customers_table.prepend( `<p >
+								<h4 class="___ " data-text="Total income"></h4>
+								<span class="bg_green_light text-light p-2">${total_income} &euro;</span> <br><br>
+								<h4 class="___ " data-text="Yearly occupancy"></h4>
+								<span class="bg_green_light text-light p-2">${( (parseInt(weeks_booked.length)  / 53) * 100).toFixed(2) } &nbsp; &percnt;</span><br>
+				
+							</p>` );
+				
 					
 					
 				}
@@ -64,6 +91,7 @@ import { translate } from "./translator/translator.js";
 			/*CLEARING customers_table , BECAUSE IF OWNER WOULD CLICK MULTIPLE TIMES
 			* ON customers BUTTON, HE WOULD SEE MULTIPLE TABLES....*/
 			customers_table.html('');
+			
 		} );
 	
 	} ) ();
