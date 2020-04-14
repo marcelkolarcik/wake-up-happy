1513372c1bac36be9142bf82d258a7771bfb7666
Marcel Kolarcik <60097861+marcelkolarcik@users.noreply.github.com>
2020-04-14 09:36:21 +0100

    updated location_autocomplete.js

--- assets/src/js/index/location_autocomplete.js
+++ assets/src/js/index/location_autocomplete.js
@@ -39,11 +39,8 @@ function autocomplete ( inp, arr )
 							b.innerHTML = "<strong>" + arr[ i ].substr ( 0, val.length ) + "</strong>";
 							b.innerHTML += arr[ i ].substr ( val.length );
 							/*insert a input field that will hold the current array item's value:*/
-							b.innerHTML += "<input type=";
-							hidden;
-							" value=";
-							" + arr[i] + ";
-							">";
+							
+							b.innerHTML +="<input type='hidden' value='" + arr[i] + "'>";
 							/*execute a function when someone clicks on the item value (DIV element):*/
 							b.addEventListener ( "click", function ()
 							{
